13a8456378cd82ba9a39f2730946d6f04690cb8f
Prepare release by ehennestad <>
2025-04-01 10:16:12 +0000

    Update version number in Contents.m

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % MatNWB : NWB For MATLAB
-% Version 2.8.6 (R2019b+) 01-Apr-2025
+% Version 2.8.7 (R2019b+) 01-Apr-2025
 %
 % Copyright (c) 2025, Neurodata Without Borders
 % ---------------------------------------------
